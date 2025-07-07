37e766e8176fe5fd06f0d4789abfdbcf9f17c498
Bughay <markschrammel6520906@gmail.com>
2025-07-07 10:52:59 +0300

    Added pydantic validation removed some useless comments

--- trainer_functions/sqlalchemy_methods.py
+++ trainer_functions/sqlalchemy_methods.py
@@ -7,16 +7,10 @@ from pydantic import BaseModel
 def add_food(food):
     Session = sessionmaker(bind=engine)
     session = Session()
+
     food_schema = FoodSchema(**food).model_dump()
     food_item = Foods(**food_schema)
-    # add_food = Foods(
-    #     food_name=food['food_name'],
-    #     calories=food['calories'],
-    #     protein=food['protein'],
-    #     carbs=food['carbs'],
-    #     fats=food['fats'],
-    #     serving_g=food['serving_g']
-    #     )
+
     session.add(food_item)
     session.commit()
 
@@ -27,11 +21,6 @@ def add_training(training):
     training_schema = TrainingSchema(**training).model_dump()
     training_item = Trainings(**training_schema)
 
-    # add_training = Trainings(
-    #     exercise_name=training['exercise_name'],
-    #     weight=training['weight'],
-    #     reps=training['reps'],
-    #     )
 
     session.add(training_item)
     session.commit()
